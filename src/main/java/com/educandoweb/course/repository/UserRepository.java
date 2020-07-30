d72b625d92e38225ee24830a31914b4e6a11c327
vanusasaravalle <vanusa.sousasaravalle@gmail.com>
2020-07-30 09:36:20 -0300

    Service

--- src/main/java/com/educandoweb/course/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.educandoweb.course.repository;
-
-import com.educandoweb.course.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
